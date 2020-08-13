d6bdc88c5d7840e5567688236cf1a9cc13dcba13
Itay Gradenwits <itay25433@gmail.com>
2020-08-13 20:06:28 +0300

    add a new kind of ErrorCode and edit the "error_getErrorMessage" according to support this kinf of error.

--- ErrorCode.c
+++ ErrorCode.c
@@ -11,6 +11,9 @@ const char* error_getErrorMessage(ErrorCode code) {
     else if (code == ERROR_MEMORY_ELLOCATION_FAIL) {
         return "memory ellocation fail";   
     }
+    else if (code == ERROR_BAD_MATRIX_SIZES) {
+        return "the sizes of the matrix are not good for this method";
+    }
     else {
         return "not a defined error";
     }
