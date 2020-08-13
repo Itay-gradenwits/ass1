e30f8f3f8cbdc0e6b148ab53ff7f9a35711297a8
Itay Gradenwits <itay25433@gmail.com>
2020-08-13 19:37:07 +0300

    implement the "error_getErrorMessage" methos from Matrix.h

--- ErrorCode.c
+++ ErrorCode.c
@@ -1,4 +1,14 @@
 #include "ErrorCode.h"
+
 bool error_isSuccess(ErrorCode code) {
     return !code;
+}
+
+const char* error_getErrorMessage(ErrorCode code) {
+    if(code == ERROR_SUCCESS) {
+        return "success";
+    }
+    else {
+        return "not a defined error";
+    }
 }
