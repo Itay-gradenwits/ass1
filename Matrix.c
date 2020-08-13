7f8380cd90271e76ac5db1bf952f35145ed09e28
Itay Gradenwits <itay25433@gmail.com>
2020-08-13 20:59:16 +0300

    implementing the "matrix create" method and changing the "matrix_destroy" method(there was a bug).

--- Matrix.c
+++ Matrix.c
@@ -1,16 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "Matrix.h"
 #include <stdint.h>
- struct Matrix {
-    int ** data;
+
+typedef struct Matrix {
+    double ** data;
     uint32_t height;
     uint32_t width;
-};
+}Matrix;
+
+
+ErrorCode matrix_create(PMatrix* matrix, uint32_t height, uint32_t width) {
+    
+    if(height < 0 || width < 0) {
+        return ERROR_BAD_MATRIX_SIZES;
+    }
+
+    *matrix = malloc(sizeof(Matrix));
+    if(*matrix == NULL) {
+        return ERROR_MEMORY_ELLOCATION_FAIL;
+    }
+    (*matrix)->height = height;
+    (*matrix)->width = width;
+
+    for(uint32_t i = 0; i < width; i ++) {
+        (*matrix)->data[i] = calloc(height, sizeof(double));
+        if((*matrix)->data[i] == NULL) {
+            for(uint32_t j = 0; j < i; j++) {
+                free((*matrix)->data[j]);
+            }
+            free((*matrix)->data);
+            free(*(matrix));
+            return ERROR_MEMORY_ELLOCATION_FAIL;
+        }
+    }
+}
 
 void matrix_destroy(PMatrix matrix) {
     for (uint32_t i = 0; i< matrix->width; i++) {
         free(matrix->data[i]);
     }
     free(matrix->data);
-    free(matrix->width);
-    free(matrix->height);
+    free(matrix);
 }
+
+
