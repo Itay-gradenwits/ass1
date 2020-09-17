ce9205042735368c0826f8dc60ab44a3761d327a
Itay Gradenwits <itay25433@gmail.com>
2020-09-17 23:44:22 +0300

    Revert "fixed what Michael asked..."
    This reverts commit bf2694f9e57294e1f1ccf8617ce49be210e2deb5.

--- ErrorCode.c
+++ ErrorCode.c
@@ -7,13 +7,13 @@ const char* error_getErrorMessage(ErrorCode code) {
     if(code == ERROR_SUCCESS) {
         return "success";
     }
-    if (code == ERROR_MEMORY_ALLOCATION_FAIL) {
+    else if (code == ERROR_MEMORY_ELLOCATION_FAIL) {
         return "memory ellocation fail";   
     }
-    if (code == ERROR_BAD_MATRIX_SIZES) {
+    else if (code == ERROR_BAD_MATRIX_SIZES) {
         return "the sizes of the matrix are not good for this method";
     }
-    if (code == ERROR_NULL_POINTER) {
+    else if (code == ERROR_NULL_POINTER) {
         return "the pointer provided is null";
     }
     else {
